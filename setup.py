16505b02ad3e66d708c3230424ba6c1bd11b58dc
syzymon <s.tworkowski@student.uw.edu.pl>
2020-11-28 14:55:24 +0100

    Add setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gin-sweep',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/Vatican-X-Formers/gin-sweep',
+    license='MIT License',
+    author='syzymon',
+    author_email='',
+    description='Creating gin configs out of .yaml sweep files',
+    python_requires='>=3.6',
+    install_requires=[
+        'pyyaml'
+    ]
+)
