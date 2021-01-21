bf2471dd3f9fbf01633c6aa7c9603ccc626e8a38
syzymon <s.tworkowski@student.uw.edu.pl>
2021-01-21 09:13:49 +0100

    Add eval ginfile generator

---
+++ gin_sweep/eval_gin.py
@@ -0,0 +1,36 @@
+import gin
+
+import argparse
+
+gin.finalize()
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--gin', type=str, required=True)
+  parser.add_argument('--eval_steps', type=int, default=49999)
+  parser.add_argument('--eval_bs', type=int, default=1)
+
+  args = parser.parse_args()
+  with gin.unlock_config():
+    gin.parse_config_file(args.gin)
+
+    n_steps = gin.query_parameter('train.steps')
+    gin.bind_parameter('train.steps', n_steps + 1)
+
+    gin.bind_parameter('batcher.eval_batch_size', args.eval_bs)
+    gin.bind_parameter('train.eval_steps', args.eval_steps)
+    gin.bind_parameter('train.eval_frequency', 1)
+
+    gin.bind_parameter('train.optimizer',
+                       gin.config.ConfigurableReference('trax.optimizers.SGD',
+                                                        False))
+    gin.bind_parameter('train.lr_schedule_fn',
+                       gin.config.ConfigurableReference(
+                           'trax.supervised.lr_schedules.constant', False))
+    gin.bind_parameter('trax.supervised.lr_schedules.constant.value', 0.0)
+
+  *filename, _ = args.gin.split('.')
+  eval_filename = '.'.join(filename) + '_eval.gin'
+
+  with open(eval_filename, 'w') as f:
+    f.write(gin.config_str())
