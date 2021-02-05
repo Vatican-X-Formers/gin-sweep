051d8d52be8fa8f01616183daafe4315571b06c9
syzymon <s.tworkowski@student.uw.edu.pl>
2021-02-05 09:15:25 +0100

    Add configurable number of steps

--- gin_sweep/eval_gin.py
+++ gin_sweep/eval_gin.py
@@ -9,18 +9,18 @@ if __name__ == '__main__':
   parser.add_argument('--gin', type=str, required=True)
   parser.add_argument('--eval_steps', type=int, default=49999)
   parser.add_argument('--eval_bs', type=int, default=1)
+  parser.add_argument('--steps', type=int, default=0)
 
   args = parser.parse_args()
   with gin.unlock_config():
     gin.parse_config_file(args.gin)
 
-    n_steps = gin.query_parameter('train.steps')
+    n_steps = args.steps or gin.query_parameter('train.steps')
     gin.bind_parameter('train.steps', n_steps + 1)
 
     gin.bind_parameter('batcher.eval_batch_size', args.eval_bs)
     gin.bind_parameter('train.eval_steps', args.eval_steps)
     gin.bind_parameter('train.eval_frequency', 1)
-    gin.bind_parameter('data_streams.shuffle_on_eval', False)
 
     gin.bind_parameter('train.optimizer',
                        gin.config.ConfigurableReference('trax.optimizers.SGD',
